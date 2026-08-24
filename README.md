404a6f13ee9d6d3fe7321cdc846bfdd466b546b6
GlassHammerMaster <h3nryr4lph@gmail.com>
2026-08-24 12:11:50 -0700

    Transferred files to new computer

---
+++ README.md
@@ -0,0 +1,2 @@
+I created this repo and website in order to learn the HTML, CSS, and JS languages to become more familiar with them and their uses in the cybersecurity industry.
+It is intended to be a resume demonstrating my skills so anyone who googles my alias will be able to see the skills I have and a live demonstration of them.
---
+++ script.js
@@ -0,0 +1,29 @@
+const NGROK_URL = "https://botanical-onset-chief.ngrok-free.dev";
+
+async function sendDataToPC() {
+    const inputField = document.getElementById("userInput");
+    const responseParagraph = document.getElementById("backendResponse");
+    const payload = {
+        user_text: inputField.value
+    };
+
+    responseParagraph.innerText = "Sending to servers";
+
+    try {
+        const response = await fetch(`${NGROK_URL}/api/submit`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "ngrok-skip-browser-warning": "true"
+            },
+            body: JSON.stringify(payload)
+        });
+        const data = await response.json();
+
+        responseParagraph.innerText = data.message;
+        inputField.value = "";
+    } catch (error) {
+        console.error("Connect Failed:", error);
+        responseParagraph.innerText = "Error: Couldn't connect to server";
+    }
+}
